4d38aeb1ffa0512106c369379c44844b537e12d3
tagasouichiro <stitow14@gmail.com>
2018-03-31 21:19:00 +0900

    Add delete

--- delete.go
+++ delete.go
@@ -1,2 +1,25 @@
 package ghttp
 
+import (
+	"net/http"
+	"time"
+)
+
+func execDelete(g GHttp) (body []byte, err error) {
+	tr := &http.Transport{
+		IdleConnTimeout: 30 * time.Second,
+	}
+	client := http.Client{
+		Transport: tr,
+	}
+	resp, err := client.Do(&http.Request{
+		Method:     DELETE,
+		RequestURI: g.getURL(),
+	})
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	return branchStatusCode(*resp)
+}
