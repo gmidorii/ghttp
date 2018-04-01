84746e47d78f8620ac4cab990672932983973c38
tagasouichiro <stitow14@gmail.com>
2018-04-01 20:04:06 +0900

    Add put

--- put.go
+++ put.go
@@ -1,5 +1,25 @@
 package ghttp
 
+import (
+	"net/http"
+	"time"
+)
+
 func execPut(g GHttp) (body []byte, err error) {
-	return
+	tr := &http.Transport{
+		IdleConnTimeout: 30 * time.Second,
+	}
+	client := http.Client{
+		Transport: tr,
+	}
+	resp, err := client.Do(&http.Request{
+		Method:     PUT,
+		RequestURI: g.getURL(),
+	})
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	return branchStatusCode(*resp)
 }
